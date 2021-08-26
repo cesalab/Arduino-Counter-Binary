e02ad87c87e962d05c04d0c16f047d92087b1794
cesalab332 <cesalab332@gmail.com>
2021-08-26 16:53:50 -0500

    first commit, ok

---
+++ README.md
@@ -0,0 +1,18 @@
+The following instructable will go through the steps to build a simple binary counter using the following materials.
+
+1 x Arduino Uno
+
+1 x Pushbutton
+
+5 x LED's (however many bits you want your counter to be)
+
+1 x 10k ohm Resistor
+
+1 x 220 ohm Resistor (possibly 2 or 3 for a larger counter)
+
+![diagram](img/diagram.png)
+
+Attached is a schematic view of the circuit that we will be using. For demonstration, I have built a 5-bit counter. You should assemble this circuit on a protoboard and attach to your Arduino.
+
+
+![picture](img/picture.png)
---
+++ binary-counter.ino
@@ -0,0 +1,64 @@
+int buttonState = 0;
+int count = 0;
+
+void setup() {
+Serial.begin(9600);
+pinMode(2,INPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(7,OUTPUT);
+}
+
+void loop() {
+digitalWrite(3, LOW);
+digitalWrite(4, LOW);
+digitalWrite(5, LOW);
+digitalWrite(6, LOW);
+digitalWrite(7, LOW);
+
+buttonState = digitalRead(2);
+
+	if(buttonState == LOW) {
+
+	} 
+	else {
+		count++;
+		delay(200);
+		Serial.print(count);
+
+		if((count % 2) > 0) {
+			digitalWrite(3, HIGH);
+		} else {
+			digitalWrite(3, LOW);
+		}
+
+		if((count % 4) > 1) {
+			digitalWrite(4, HIGH);
+		} else {
+			digitalWrite(4, LOW);
+		}
+
+		if((count % 8) > 3) {
+			digitalWrite(5, HIGH);
+		} else {
+			digitalWrite(5, LOW);
+		}
+
+		if((count % 16) > 7) {
+			digitalWrite(6, HIGH);
+		} else {
+			digitalWrite(6, LOW);
+		}
+
+		if((count % 32) > 15) {
+			digitalWrite(7, HIGH);
+		} else {
+			digitalWrite(7, LOW);
+		}
+
+		delay(200);
+	}
+  
+}
